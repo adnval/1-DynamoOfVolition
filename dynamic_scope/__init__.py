43ace20e9651815f73fdab1ea22f5dced5d7d57b
Kevin <kevin6700@proton.me>
2024-06-12 12:41:14 -0400

    refactor: changed the definitions of the DynamicScope methods to use dict's methods

--- dynamic_scope/__init__.py
+++ dynamic_scope/__init__.py
@@ -10,15 +10,23 @@ class DynamicScope(abc.Mapping):
 
     # Method to access the value given a key
     def __getitem__(self, key: str) -> Optional[Any]:
-        return self.env[key]
+        return self.env.__getitem__(key)
+
+    # Method to set a key-value pair in the reference environment
+    def __setitem__(self, key: str, value: Optional[Any]):
+        self.env.__setitem__(key, value)
+
+    # Method to delete a key-value pair in the reference environment
+    def __delitem__(self, key: str):
+        self.env.__delitem__(key)
 
     # Method to return an interator object for the reference environment
-    def __iter__(self) -> Any:
-        return iter(self.env)
+    def __iter__(self) -> Iterator:
+        return self.env.__iter__()
 
     # Method to return the amount of items for the reference environment
     def __len__(self) -> int:
-        return len(self.env)
+        return self.env.__len__()
 
 
 def get_dynamic_re() -> DynamicScope:
