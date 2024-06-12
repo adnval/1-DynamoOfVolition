74fece877534bc0cde75912473b12cf1abd3990c
Kevin <kevin6700@proton.me>
2024-06-11 21:26:50 -0400

    refactor: added a submission init.py in the main directory

---
+++ __init__.py
@@ -0,0 +1,13 @@
+import inspect
+from collections import abc
+from types import FunctionType
+from typing import Any, Dict, Iterator, Optional
+
+
+class DynamicScope(abc.Mapping):
+    def __init__(self):
+        self.env: Dict[str, Optional[Any]] = {}
+
+
+def get_dynamic_re() -> DynamicScope:
+    return None
