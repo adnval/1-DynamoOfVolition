3337ec0e1f468222b9a75b14e019eee521295b8e
Kevin <kevin6700@proton.me>
2024-06-12 12:06:14 -0400

    feat: added methods for the DynamicScope class

--- dynamic_scope/__init__.py
+++ dynamic_scope/__init__.py
@@ -1,13 +1,26 @@
-from typing import Dict, Any, Iterator, Optional
+import inspect
 from collections import abc
 from types import FunctionType
-import inspect
+from typing import Any, Dict, Iterator, Optional
 
 
 class DynamicScope(abc.Mapping):
     def __init__(self):
         self.env: Dict[str, Optional[Any]] = {}
 
+    # Method to access the value given a key
+    def __getitem__(self, key: str) -> Optional[Any]:
+        return self.env[key]
+
+    # Method to return an interator object for the reference environment
+    def __iter__(self) -> Any:
+        return iter(self.env)
+
+    # Method to return the amount of items for the reference environment
+    def __len__(self) -> int:
+        return len(self.env)
+
 
 def get_dynamic_re() -> DynamicScope:
-    return None
+    referenceEnvironment = DynamicScope()
+    return referenceEnvironment
