9418afcec14e76ea89f4954741e10d5338769700
Kevin <kevin6700@proton.me>
2024-06-12 23:52:29 -0400

    revert: rollbacked to debug

--- dynamic_scope/__init__.py
+++ dynamic_scope/__init__.py
@@ -10,13 +10,9 @@ class DynamicScope(abc.Mapping):
 
     # Method to access the value given a key
     def __getitem__(self, key: str) -> Optional[Any]:
-        # if not key in self.env:
-        #     raise NameError(f"Key '{key}' does not exist in DynamicScope object")
-        # return self.env.__getitem__(key)
-        if key in self.env:
-            return self.env[key]
-        else:
-            raise NameError(f"Key '{key}' does not exist in DynamicScope object")
+        if not key in self.env:
+            raise NameError(f"Name '{key}' is not defined")
+        return self.env.__getitem__(key)
 
     # Method to set a key-value pair in the reference environment
     def __setitem__(self, key: str, value: Optional[Any]):
@@ -43,7 +39,7 @@ def get_dynamic_re() -> DynamicScope:
         vars = frameObject.f_locals
         freeVars = frameObject.f_code.co_freevars
         for var in vars:
-            if var in refEnvironment or vars[var] is None or var in freeVars:
+            if var in refEnvironment.env or vars[var] is None or var in freeVars:
                 continue
             refEnvironment[var] = vars[var]
     return refEnvironment
