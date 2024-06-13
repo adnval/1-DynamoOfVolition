5bb2fa899b9cdca9fea74d3834a84507ed3f27a2
Kevin <kevin6700@proton.me>
2024-06-13 01:13:43 -0400

    style: added docstrings and comments in compliance with PEP 8

--- dynamic_scope/__init__.py
+++ dynamic_scope/__init__.py
@@ -5,40 +5,52 @@ from typing import Any, Dict, Iterator, Optional
 
 
 class DynamicScope(abc.Mapping):
+    """Encapsulates a dynamic reference environment.
+
+    Defines a dictionary of variable names and values at a given statement.
+    """
+
     def __init__(self):
         self.env: Dict[str, Optional[Any]] = {}
 
-    # Method to access the value given a key
+    """Returns the value of the key."""
+
     def __getitem__(self, key: str) -> Optional[Any]:
+        # Handle nonexistent variables that are attempting to be accessed
         if not key in self.env:
             raise NameError(f"Name '{key}' is not defined")
         return self.env.__getitem__(key)
 
-    # Method to set a key-value pair in the reference environment
+    """Adds a key-value pair to the reference environment."""
+
     def __setitem__(self, key: str, value: Optional[Any]):
         self.env.__setitem__(key, value)
 
-    # Method to delete a key-value pair in the reference environment
+    """Deletes a key-value pair from the reference environment."""
+
     def __delitem__(self, key: str):
         self.env.__delitem__(key)
 
-    # Method to return an interator object for the reference environment
+    """Returns an iterator for the reference environment."""
+
     def __iter__(self) -> Iterator:
         return self.env.__iter__()
 
-    # Method to return the amount of items for the reference environment
+    """Returns the length of the reference environment."""
+
     def __len__(self) -> int:
         return self.env.__len__()
 
 
 def get_dynamic_re() -> DynamicScope:
     refEnvironment = DynamicScope()
+    # Starts iterating from index 1 for get_dynamic_re to skip itself
     for frameInfo in inspect.stack()[1:]:
         frameObject = frameInfo[0]
-        functionName = frameObject.f_code.co_name
         vars = frameObject.f_locals
         freeVars = frameObject.f_code.co_freevars
         for var in vars:
+            # Ignores existing, none-valued, and free variables respectively
             if var in refEnvironment.env or vars[var] is None or var in freeVars:
                 continue
             refEnvironment[var] = vars[var]
